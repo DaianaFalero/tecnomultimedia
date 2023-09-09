d7ea0dc992d04a8f8a8368f4f33e85458fcb25c8
DaianaFalero <129103315+DaianaFalero@users.noreply.github.com>
2023-09-09 17:02:00 -0300

    Update tpprueba.js

--- tpprueba/tpprueba.js
+++ tpprueba/tpprueba.js
@@ -6,9 +6,9 @@ let estado="inicio";
 let fondo,btn,pos1=100,pos2=100,x=300,y=200,dirX = 1, dirY=-1,p1=0, p2=0;
 let bx1=50;
 let bx2=200;
-let bx3=500;
+let bx3=460;//INICIAR
 let by1=280
-let by2=300
+let by2=280//INICIAR
 
 
 
@@ -27,7 +27,8 @@ function clic(x, y){
  
   let ok=false;
  
-   if(dist(x,y,mouseX,mouseY) < 45){
+   if(dist(x,y,mouseX,mouseY) < 40){
+     //(mouseX>0 && mouseX<100 && mouseY>380 && mouseY<480) 
          ok = true;
          }
  return ok;
@@ -43,29 +44,29 @@ function jugar(){
   line(300,0,300,400);
   circle(x,y, 11,11);
  
-    if(keyIsDown(65))
+    if(keyIsDown(65))// ascii A
      {
       pos1 = pos1 - 4;
      }
-  if(keyIsDown(90))
+  if(keyIsDown(90))//ascii Z
      {
       pos1 = pos1 + 4;
      }
  
  
  
-  if(keyIsDown(UP_ARROW))
+  if(keyIsDown(UP_ARROW))//flecha arriba
      {
       pos2 = pos2 - 4;
      }
-  if(keyIsDown(DOWN_ARROW))
+  if(keyIsDown(DOWN_ARROW))//flecha abajo
      {
       pos2 = pos2 + 4;
      }
  
  
-   x = x+(2 * dirX);
-  y = y+(2 * dirY);
+   x = x+(2.5 * dirX); // velocidad de la pelota en x
+  y = y+(2.5 * dirY);// velocidad en y
  
   if(y > height)
     {
@@ -121,19 +122,22 @@ function draw() {
 
   if(estado == "inicio"){
     image(fondo,0,0);
-    image(btn,460,280,120,100);
+    image(btn,bx3,by2,120,100);
     textSize(25);
     fill(255,255,255);
     text ("INICIAR",480,340);
+    
     image(btn,50,300,120,70);
      textSize(20);
     fill(255,255,255);
     text ("Creditos",80,340);
+    
     image(btn,200,300,120,70);
      textSize(20);
     fill(255,255,255);
     text ("Reglas",230,340);
     
+   // ellipse(515,330,120,100);
     fill(254,150,247);
     textSize(150);
     text("PONG",80,height/2);
@@ -145,13 +149,17 @@ function draw() {
      
     } else
     if(estado == "creditos" ){
-      background(255,3,9);
-      textSize(10);
-      text('Juego originalmente creado por Allan Alcorn', width/3, height/2);
+      background(254,150,247);
+      textSize(20);
+      fill(255,255,255);
+      text('Juego originalmente creado por Allan Alcorn, este videojuego\n  fue programado por Falero Daiana',20,height/6);
      
     }else 
     if(estado == "reglas" ){
       background(254,150,247);
+      textSize(20);
+      fill(255.255,255);
+      text("las reglas del juego son:",50,50);
     } else
       if(estado == "fin"){
          background(254,150,247);
@@ -168,6 +176,22 @@ function draw() {
   }  
 }
 
+  
+function mousePressed() {
+  if (estado == "inicio") {
+    if (clic(bx3, by2)) {
+      estado = "juego";
+    } else if (clic(bx1, by1)) {
+      estado = "creditos";
+    }else if (clic(bx2,by1)){
+      estado = "reglas";
+    } else if (estado == "fin") {
+      estado = "inicio";
+      p1 = 0;
+      p2 = 0;
+    }
+  }
+}
   
 function mousePressed() {
   if (estado == "inicio") {
