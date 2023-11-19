6b26d03309ad532013c27c4e2315a9e75d66c3b4
DaianaFalero <129103315+DaianaFalero@users.noreply.github.com>
2023-11-18 23:25:55 -0300

    Add files via upload

---
+++ moustro.js
@@ -0,0 +1,41 @@
+class Moustro{
+  constructor() {
+    this.px = width/2;
+    this.py = height+40;
+    this.dir = 'aba';  // Dirección inicial del monstruo ('aba': hacia abajo, 'arr': hacia arriba)
+    this.cant = 0; // contador
+    this.img = loadImage('./data/monstruo.png');
+  }
+
+  colision(x_, y_) {
+    if(dist(x_, y_, this.px+45, this.py+60) < 30){ // Si hay colisión, reiniciar la posición vertical del monstruo, incrementar el contador y mostrarlo en la consola
+      this.py = height+40;
+      this.cant++;
+      console.log(this.cant);
+     
+  }
+  }
+
+  cant(){   // Método para obtener la cantidad de colisiones con el monstruo
+   return cant;
+  }
+ 
+  display() {
+     text(this.cant, 70, 70);
+   image(this.img, this.px, this.py, 90,120);
+    if(this.py > height){
+      this.dir = 'arr'; // Si el monstruo llega al borde inferior, cambiar la dirección a 'arr' (arriba)
+    }else
+     if(this.py < 0){
+        this.dir = 'aba'; //abajo
+     }
+     
+     // Actualizar la posición vertical del monstruo según su dirección
+    if(this.dir == 'aba'){
+      this.py = this.py + 1.5;
+    }else{
+      this.py = this.py - 1.5;
+    }
+  }  
+ 
+}  
---
+++ tp5.js
@@ -0,0 +1,36 @@
+
+let estado="inicio",t;
+let mouns,jue,tim;
+
+function preload(){
+    this.fondo = loadImage('./data/oceano.jpg');
+}
+
+function setup() {
+createCanvas(600, 400);
+mouns = new Moustro();
+jue = new Juego();
+tim = new Tiempo();
+}
+
+
+function draw(){
+ 
+ 
+ 
+  if(estado=="inicio"){
+image(fondo,0,0,600,400);
+jue.display();
+  jue.teclado();
+  mouns.display();
+  mouns.colision(jue.posX(), jue.posY());
+  tim.display();
+  t=tim.time1();
+  if(t > 260 ){
+    estado="perdieron";
+  }
+  }
+   if(estado=="perdieron"){
+      background(0,0,100);
+   }  
+}  
